c71aee1457b0383b967a42c583477c0b7c501828
Adhiponnuzz <adithyaminnu689693@gmail.com>
2022-09-20 09:53:42 +0530

    commit

---
+++ lib/sample.dart
@@ -0,0 +1,20 @@
+void main()
+{
+  String name="Adhi";
+  int age=20;
+  String mail="adhi123@gmail.com";
+  double mark=88.6;
+  String clg="MZCE";
+  int phone=9988776655;
+  String department="MCA";
+  print('my name is ${name}');
+  print('iam ${age} years old ');
+  print('${mail}');
+  print('my ${mark} is');
+  print('college name is ${clg}');
+  print('phone number is ${phone}');
+  print('${department}');
+
+
+
+}
