8c805ec3416cfddb17c02c803e09c1dd4f982014
Adhiponnuzz <adithyaminnu689693@gmail.com>
2022-09-21 13:53:19 +0530

    commit

---
+++ lib/details.dart
@@ -0,0 +1,14 @@
+void showmydetails(String name,int age,String email,int phone,String qualification){
+  print('Name         = ${name}');
+  print('Age          = ${age}');
+  print('Email        = ${email}');
+  print('Phone        = ${phone}');
+  print('Qualification= ${qualification}');
+
+}
+void main(){
+  showmydetails('Adithya',23,'adhi.com',9977665544,'mca');
+
+}
+
+
---
+++ lib/function.dart
@@ -0,0 +1,29 @@
+void add(int a,int b){
+  print('sum is   : ${a+b}');
+}
+void sub(int a,int b){
+
+  print('value is : ${a-b}');
+
+}
+void div(int a,int b){
+
+  print('value is :${a/b}');
+}
+void mul(int a,int b){
+
+  print('value is : ${a*b}');
+
+}
+
+
+
+void main(){
+  add(22,12);
+  sub(30,20);
+  div(16,2);
+  mul(4,5);
+
+
+
+}
